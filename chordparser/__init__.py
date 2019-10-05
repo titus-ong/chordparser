4cb023bfa16464e0bcf030a899d5e76981299420
Titus Ong <55629508+titus-ong@users.noreply.github.com>
2019-10-05 21:56:28 +0100

    added init and chordsheet

--- chordparser/__init__.py
+++
@@ -1,6 +0,0 @@
-from .chordsheet import Sheet
-
-def create(file_path):
-    with open(file_path, 'r') as f:
-        contents = f.readlines()
-    return Sheet(contents)
--- chordparser/chordsheet.py
+++
@@ -1,3 +0,0 @@
-class Sheet:
-    def __init__(self, contents):
-        self.contents = contents

c092accf57ae81e3694d2f9b03b0b3d7bdf921ca
Titus Ong <55629508+titus-ong@users.noreply.github.com>
2019-10-05 21:57:35 +0100

    edit setup.py for more accurate description, test suites

--- setup.py
+++ setup.py
@@ -1,14 +1,28 @@
 from setuptools import setup
 
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
 setup(name='chordparser',
       version='1.0',
       description='Parse .cho (ChordPro) files',
+      long_description=readme(),
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.7',
+          'Topic :: Text Processing :: Markup',
+          ],
+      keywords='chord sheet music parse',
       url='http://github.com/titus-ong/chordparser',
       author='Titus Ong',
       author_email='titusongyl@gmail.com',
       license='MIT',
       packages=['chordparser'],
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest'],
       install_requires=[
-          're',
+          'pytest',
           ],
       zip_safe=False)

f51ac818b6e9a9433593ce6210540492a838741d
Titus Ong <55629508+titus-ong@users.noreply.github.com>
2019-10-10 22:46:23 +0100

    remove unnecessary assert messages

--- tests/test_create.py
+++ tests/test_create.py
@@ -7,16 +7,16 @@ import pytest
 
 def test_create():
     sheet = cp.create("tests/sample_sheet.cho")
-    assert isinstance(sheet, cp.Sheet), "Sheet was not created from .cho file"
+    assert isinstance(sheet, cp.Sheet)
 
 
 def test_create_contents():
     sheet = cp.create("tests/sample_sheet.cho")
     with open("tests/sample_sheet.cho", 'r') as f:
         contents = f.readlines()
-    assert sheet.contents_raw == contents, "Sheet contents are incorrect"
+    assert sheet.contents_raw == contents
 
 
 def test_create_invalid_format():
-    with pytest.raises(cp.chordsheet.ParseError):
+    with pytest.raises(cp.ParseError):
         sheet = cp.create("tests/")
--- tests/test_scales.py
+++ tests/test_scales.py
@@ -1,4 +1,3 @@
-import chordparser as cp
 import chordparser.scales as scales
 import pytest
 
@@ -6,9 +5,9 @@ import pytest
 @pytest.mark.parametrize(
     "mode, intervals", [
          ("ionian", (2, 2, 1, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 1)),
-         ("major", (2, 2, 1, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 1)),
+         ("MAJOR", (2, 2, 1, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 1)),
          ("dorian", (2, 1, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 1, 2)),
-         ("phrygian", (1, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 1, 2, 2)),
+         ("PhRyGiAn", (1, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 1, 2, 2)),
          ("lydian", (2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 1, 2, 2, 1)),
          ("mixolydian", (2, 2, 1, 2, 2, 1, 2, 2, 2, 1, 2, 2, 1, 2)),
          ("aeolian", (2, 1, 2, 2, 1, 2, 2, 2, 1, 2, 2, 1, 2, 2)),
@@ -18,16 +17,13 @@ import pytest
     )
 def test_scale_modes(mode, intervals):
     scale = scales.Scale('C', mode)
-    string = (
-        f'Scale intervals for {mode} are incorrect. '
-        + f'Expected {intervals} but got {scale.intervals} instead'
-        )
-    assert scale.intervals == intervals, string
+    assert scale.intervals == intervals
 
-
-def test_scale_modes_fake():
+@pytest.mark.parametrize(
+    "mode", ["ionia", 1, True])
+def test_scale_modes_fake(mode):
     with pytest.raises(scales.ModeError):
-        scales.Scale('C', "ionia")
+        scales.Scale('C', mode)
 
 
 @pytest.mark.parametrize(
@@ -50,11 +46,7 @@ def test_scale_modes_fake():
     )
 def test_scale_note_order(key, note_order):
     scale = scales.Scale(key)
-    string = (
-        f'Scale intervals for {key} are incorrect. '
-        + f'Expected {note_order} but got {scale._note_order} instead'
-        )
-    assert scale._note_order == note_order, string
+    assert scale._note_order == note_order
 
 
 @pytest.mark.parametrize(
@@ -69,13 +61,11 @@ def test_scale_note_order(key, note_order):
             'F', 'G', 'A', 'B\u266d', 'C', 'D', 'E\u266d',
             'F', 'G', 'A', 'B\u266d', 'C', 'D', 'E\u266d')),
         ('G\u266d', 'dorian', (
-            'G\u266d', 'A\u266d', 'B\U0001D12B', 'C\u266d', 'D\u266d', 'E\u266d', 'F\u266d',
-            'G\u266d', 'A\u266d', 'B\U0001D12B', 'C\u266d', 'D\u266d', 'E\u266d', 'F\u266d')),
+            'G\u266d', 'A\u266d', 'B\U0001D12B',
+            'C\u266d', 'D\u266d', 'E\u266d', 'F\u266d',
+            'G\u266d', 'A\u266d', 'B\U0001D12B',
+            'C\u266d', 'D\u266d', 'E\u266d', 'F\u266d')),
         ])
 def test_scale_notes(key, mode, notes):
     scale = scales.Scale(key, mode)
-    string = (
-        f'Scale intervals for {key} {mode} are incorrect. '
-        + f'Expected {notes} but got {scale.notes} instead'
-        )
-    assert scale.notes == notes, string
+    assert scale.notes == notes

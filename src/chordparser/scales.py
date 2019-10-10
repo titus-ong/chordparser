df513f6246e6d2efa8d65446d428fcb6dd2f5d8b
Titus Ong <55629508+titus-ong@users.noreply.github.com>
2019-10-10 01:40:15 +0100

    create scale notes and tests for them

--- src/chordparser/scales.py
+++ src/chordparser/scales.py
@@ -2,33 +2,99 @@
 Basis of chords. Only diatonic scales are included.
 """
 from .general import Error
+import re
 
 
 class ModeError(Error):
     pass
 
 
+class KeySignatureError(Error):
+    pass
+
+
 class Scale:
     _heptatonic_base = (2, 2, 1, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 1)
     _SCALES = {
+        "major": 0,
         "ionian": 0,
         "dorian": 1,
         "phrygian": 2,
         "lydian": 3,
         "mixolydian": 4,
         "aeolian": 5,
+        "minor": 5,
         "locrian": 6,
         }
-    _notes = ('C', 'D', 'E', 'F', 'G', 'A', 'B')
+    _SCALE_DEGREE = {
+        0: "ionian",
+        1: "dorian",
+        2: "phrygian",
+        3: "lydian",
+        4: "mixolydian",
+        5: "aeolian",
+        6: "locrian",
+    }
+    _notes = ('C', 'D', 'E', 'F', 'G', 'A', 'B',
+              'C', 'D', 'E', 'F', 'G', 'A', 'B')
+    _flat = '\u266d'
+    _sharp = '\u266f'
+    _doubleflat = '\U0001D12B'
+    _doublesharp = '\U0001D12A'
+    _symbols = {
+        _flat: -1, _doubleflat: -2,
+        _sharp: +1, _doublesharp: +2,
+        -1: _flat, -2: _doubleflat,
+        +1: _sharp, +2: _doublesharp,
+        0: '',
+        }
 
-    def __init__(self, mode="ionian"):
+    def __init__(self, key, mode="major"):
+        self.key = key  # Implement method to check if key is valid
+        # How to know Cmaj7 is major and Cm7 is minor?
         self.mode = mode
         self.intervals = self._get_scale_intervals()
+        self.notes = self._get_notes()
 
-    def _get_scale_intervals(self):
+    def _get_scale_intervals(self, custom_mode=None):
+        mode = custom_mode or self.mode
         try:
-            shift = Scale._SCALES[self.mode.lower()]
+            shift = Scale._SCALES[mode.lower()]
         except KeyError:
             raise ModeError("Mode cannot be found")
-        scale_intervals = Scale._heptatonic_base[shift:] + Scale._heptatonic_base[:shift]
+        scale_intervals = (
+            Scale._heptatonic_base[shift:]
+            + Scale._heptatonic_base[:shift]
+            )
         return scale_intervals
+
+    def _get_notes(self):
+        if not re.match(
+                        '^[a-gA-G](\u266F|\u266D|\U0001D12B|\U0001D12A){0,1}$',
+                        self.key, re.UNICODE):
+            raise KeySignatureError("Key signature does not exist")
+        self.idx = Scale._notes.index(self.key[0])
+        self._note_order = self._get_note_order()
+        return self._add_note_symbols()
+
+    def _get_note_order(self):
+        note_order = Scale._notes[self.idx:] + Scale._notes[:self.idx]
+        return note_order
+
+    def _add_note_symbols(self):
+        base_intervals = self._get_scale_intervals(
+                Scale._SCALE_DEGREE[self.idx]
+                )
+        if len(self.key) > 1:
+            symbol_increment = Scale._symbols[self.key[1]]
+        else:
+            symbol_increment = 0
+        note_list = []
+        for num, note in enumerate(self._note_order):
+            total_increment = (
+                symbol_increment
+                + sum(self.intervals[:num])
+                - sum(base_intervals[:num])
+                )
+            note_list.append(note + Scale._symbols[total_increment])
+        return tuple(note_list)
--- tests/test_scales.py
+++ tests/test_scales.py
@@ -1,23 +1,69 @@
 import chordparser as cp
+import chordparser.scales as scales
 import pytest
 
 
 @pytest.mark.parametrize(
     "mode, intervals", [
          ("ionian", (2, 2, 1, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 1)),
+         ("major", (2, 2, 1, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 1)),
          ("dorian", (2, 1, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 1, 2)),
          ("phrygian", (1, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 1, 2, 2)),
          ("lydian", (2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 1, 2, 2, 1)),
          ("mixolydian", (2, 2, 1, 2, 2, 1, 2, 2, 2, 1, 2, 2, 1, 2)),
          ("aeolian", (2, 1, 2, 2, 1, 2, 2, 2, 1, 2, 2, 1, 2, 2)),
+         ("minor", (2, 1, 2, 2, 1, 2, 2, 2, 1, 2, 2, 1, 2, 2)),
          ("locrian", (1, 2, 2, 1, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2)),
          ]
     )
 def test_scale_modes(mode, intervals):
-    scale = cp.scales.Scale(mode)
+    scale = scales.Scale('C', mode)
     assert scale.intervals == intervals, "Scale intervals are incorrect"
 
 
 def test_scale_modes_fake():
-    with pytest.raises(cp.scales.ModeError):
-        cp.scales.Scale("ionia")
+    with pytest.raises(scales.ModeError):
+        scales.Scale('C', "ionia")
+
+
+@pytest.mark.parametrize(
+    "key, note_order", [
+        ('C', ('C', 'D', 'E', 'F', 'G', 'A', 'B',
+               'C', 'D', 'E', 'F', 'G', 'A', 'B')),
+        ('D', ('D', 'E', 'F', 'G', 'A', 'B', 'C',
+               'D', 'E', 'F', 'G', 'A', 'B', 'C')),
+        ('E', ('E', 'F', 'G', 'A', 'B', 'C', 'D',
+               'E', 'F', 'G', 'A', 'B', 'C', 'D')),
+        ('F', ('F', 'G', 'A', 'B', 'C', 'D', 'E',
+               'F', 'G', 'A', 'B', 'C', 'D', 'E')),
+        ('G', ('G', 'A', 'B', 'C', 'D', 'E', 'F',
+               'G', 'A', 'B', 'C', 'D', 'E', 'F')),
+        ('A', ('A', 'B', 'C', 'D', 'E', 'F', 'G',
+               'A', 'B', 'C', 'D', 'E', 'F', 'G')),
+        ('B', ('B', 'C', 'D', 'E', 'F', 'G', 'A',
+               'B', 'C', 'D', 'E', 'F', 'G', 'A')),
+        ]
+    )
+def test_scale_note_order(key, note_order):
+    scale = scales.Scale(key)
+    assert scale._note_order == note_order, "Scale note order is incorrect"
+
+
+@pytest.mark.parametrize(
+    "key, mode, notes", [
+        ('C', 'major', (
+            'C', 'D', 'E', 'F', 'G', 'A', 'B',
+            'C', 'D', 'E', 'F', 'G', 'A', 'B')),
+        ('C\u266f', 'minor', (
+            'C\u266f', 'D\u266f', 'E', 'F\u266f', 'G\u266f', 'A', 'B',
+            'C\u266f', 'D\u266f', 'E', 'F\u266f', 'G\u266f', 'A', 'B')),
+        ('F', 'mixolydian', (
+            'F', 'G', 'A', 'B\u266d', 'C', 'D', 'E\u266d',
+            'F', 'G', 'A', 'B\u266d', 'C', 'D', 'E\u266d')),
+        ('G\u266d', 'dorian', (
+            'G\u266d', 'A\u266d', 'B\U0001D12B', 'C\u266d', 'D\u266d', 'E\u266d', 'F\u266d',
+            'G\u266d', 'A\u266d', 'B\U0001D12B', 'C\u266d', 'D\u266d', 'E\u266d', 'F\u266d')),
+        ])
+def test_scale_notes(key, mode, notes):
+    scale = scales.Scale(key, mode)
+    assert scale.notes == notes, "Scale notes are incorrect"

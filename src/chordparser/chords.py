5daec3af91b3c2885cbb61d681b7778463cd6150
Titus Ong <55629508+titus-ong@users.noreply.github.com>
2019-10-11 17:03:27 +0100

    clean up code for regex, add transpose function with tests

--- src/chordparser/chords.py
+++ src/chordparser/chords.py
@@ -1,8 +1,9 @@
 """
 Parse chord notation and return key and mode.
 """
-from .general import Error
-from .notes import Note
+from .general import Error, TransposeError
+from .notes import Note, Key
+from typing import Union
 import re
 
 
@@ -11,7 +12,7 @@ class ChordError(Error):
 
 
 class Chord:
-    def __init__(self, chord_name: str):
+    def __init__(self, chord_name: Union[Note, str]):
         self.name = chord_name
 
     @property
@@ -20,34 +21,60 @@ class Chord:
 
     @name.setter
     def name(self, value):
-        if not isinstance(value, str):
-            raise ChordError("Only strings are accepted")
-        pattern = "([a-gA-G])(bb|##|b|#){0,1}((Maj|Ma|M|maj)|(min|m|-)){0,1}(.*)"
-        rgx = re.match(pattern, value)
+        if not (isinstance(value, str) or isinstance(value, Note)):
+            raise ChordError("Only strings or Notes are accepted")
+        if isinstance(value, Note):
+            value = str(value)
+        pattern = (
+            "([a-gA-G])(\u266F|\u266D|\U0001D12B|\U0001D12A|bb|##|b|#)"
+            "{0,1}((Maj|Ma|M|maj)|(min|m|-)){0,1}(.*)"
+            )
+        rgx = re.match(pattern, value, re.UNICODE)
         if not rgx:
             raise ChordError("Chord could not be recognised")
+        self.key = self._parse_key(rgx)
+        self.quality = self._parse_quality(rgx)
+        self.other = self._parse_other(rgx)
+        self.major_minor = self._maj_min_str(rgx)
+        self._name = str(self.key) + self.major_minor + self.other
+
+    def _parse_key(self, rgx):
         note = rgx.group(1)
         accidental = rgx.group(2) or ''
-        self.note = Note(note + accidental)
-        if not rgx.group(3):  # No quality, check capital letter
-            quality = rgx.group(1).isupper()
-        elif rgx.group(4):  # Major
-            quality = True
+        return Key(note + accidental)
+
+    def _parse_quality(self, rgx):
+        if not rgx.group(3) and rgx.group(1).isupper():
+            # No quality, capital letter key
+            return 'major'
+        elif not rgx.group(3):
+            # lowercase key
+            return 'minor'
+        elif rgx.group(4):
+            return 'major'
         else:
-            quality = False
-        if quality:
-            self.quality = 'major'
+            return 'minor'
+
+    def _parse_other(self, rgx):
+        return rgx.group(6)
+
+    def _maj_min_str(self, rgx):
+        """Print major if seventh chord."""
+        if self.quality == 'minor':
+            return 'm'
+        if rgx.group(4) and re.match('7', self.other):
+            return rgx.group(4)
         else:
-            self.quality = 'minor'
-        self.other = rgx.group(6)
-        self._name = value
+            return ''
 
+    def transpose(self, value: int = 0, flats=False):
+        if not isinstance(value, int):
+            raise TransposeError("Only integers are accepted")
+        if flats:
+            self.key.use_flats()
+        self.key.transpose(value)
+        self.name = str(self.key) + self.major_minor + self.other
+        return self
 
-class ChordConverter:
-    pass  # Convert chords to scales (keys) for roman numeral analysis
-
-
-class ChordTransposer:
-    pass  # parse chord to get position relative to base interval (if there's sharp/flat), then go to new key and add that position back to the sign
-    # e.g. D# in G major --> +1 to D in G major
-    # convert to Eb major --> Bb +1 ==> B in Eb major
+    def __repr__(self):
+        return f'{self.name} chord'
--- tests/test_chords.py
+++ tests/test_chords.py
@@ -1,13 +1,16 @@
 import chordparser.chords as chords
+from chordparser.notes import Note, Key
+from chordparser.general import TransposeError
 import pytest
 
 
 @pytest.mark.parametrize(
     "name, chord", [
-        ("C", "C"), ("DbM", "DbM"), ("Dmaj", "Dmaj"), ("D#Ma7", "D#Ma7"),
-        ("Em", "Em"), ("f", "f"), ("F#-", "F#-"), ("G7", "G7"),
-        ("Abmin7", "Abmin7"), ("Bbbaug", "Bbbaug"), ("A##5", "A##5"),
-        ("Bdim", "Bdim"), ("Cmin7dim5", "Cmin7dim5")])
+        (Note("C"), "C"), (Key("C#"), "C#"), ("DbM", "D\u266d"), ("Dmaj", "D"),
+        ("D#Ma7", "D\u266fMa7"), ("Em", "Em"), ("f", "Fm"),
+        ("F#-", "F\u266fm"), ("G7", "G7"),
+        ("Abmin7", "A\u266dm7"), ("Bbbaug", "B\U0001D12Baug"),
+        ("A##5", "A\U0001D12A5"), ("Bdim", "Bdim"), ("Cmin7dim5", "Cm7dim5")])
 def test_chord_name(name, chord):
     new_chord = chords.Chord(name)
     assert new_chord.name == chord
@@ -15,22 +18,85 @@ def test_chord_name(name, chord):
 
 @pytest.mark.parametrize(
     "name, note", [
-        ("C", "C"), ("DbM", "D\u266d"), ("Dmaj", "D"), ("D#Ma7", "D\u266f"),
-        ("Em", "E"), ("f", "F"), ("F#-", "F\u266f"), ("G7", "G"),
+        (Note("C"), "C"), (Key("C#"), "C\u266f"), ("DbM", "D\u266d"),
+        ("Dmaj", "D"), ("D#Ma7", "D\u266f"), ("Em", "E"), ("f", "F"),
+        ("F#-", "F\u266f"), ("G7", "G"),
         ("Abmin7", "A\u266d"), ("Bbbaug", "B\U0001D12B"),
         ("A##5", "A\U0001D12A"), ("Bdim", "B"), ("Cmin7dim5", "C")])
 def test_chord_note(name, note):
     new_chord = chords.Chord(name)
-    assert new_chord.note.value == note
+    assert new_chord.key == note
 
 
 # Only major and minor supported (not diminished/augmented)
 @pytest.mark.parametrize(
     "name, quality", [
-        ("C", "major"), ("DbM", "major"), ("Dmaj", "major"), ("D#Ma7", "major"),
+        (Note("C"), "major"), (Key("C#"), "major"), ("DbM", "major"),
+        ("Dmaj", "major"), ("D#Ma7", "major"),
         ("Em", "minor"), ("f", "minor"), ("F#-", "minor"), ("G7", "major"),
         ("Abmin7", "minor"), ("Bbbaug", "major"), ("A##5", "major"),
         ("Bdim", "major"), ("Cmin7dim5", "minor")])
 def test_chord_quality(name, quality):
     new_chord = chords.Chord(name)
     assert new_chord.quality == quality
+
+
+@pytest.mark.parametrize(
+    "name, other", [
+        (Note("C"), ""), (Key("C#"), ""), ("DbM", ""), ("Dmaj", ""),
+        ("D#Ma7", "7"), ("Em", ""), ("f", ""), ("F#-", ""), ("G7", "7"),
+        ("Abmin7", "7"), ("Bbbaug", "aug"),
+        ("A##5", "5"), ("Bdim", "dim"), ("Cmin7dim5", "7dim5")])
+def test_chord_other(name, other):
+    new_chord = chords.Chord(name)
+    assert new_chord.other == other
+
+
+@pytest.mark.parametrize(
+    "name, major_minor", [
+        (Note("C"), ""), (Key("C#"), ""), ("DbM", ""), ("Dmaj", ""),
+        ("D#Ma7", "Ma"), ("Em", "m"), ("f", "m"), ("F#-", "m"), ("G7", ""),
+        ("Abmin7", "m"), ("Bbbaug", ""),
+        ("A##5", ""), ("Bdim", ""), ("Cmin7dim5", "m")])
+def test_chord_major_minor(name, major_minor):
+    new_chord = chords.Chord(name)
+    assert new_chord.major_minor == major_minor
+
+
+@pytest.mark.parametrize(
+    "name, chord", [
+        (Note("C"), "C chord"), (Key("C#"), "C\u266f chord"),
+        ("DbM", "D\u266d chord"), ("Dmaj", "D chord"),
+        ("D#Ma7", "D\u266fMa7 chord"), ("A##5", "A\U0001D12A5 chord"),
+        ("Bdim", "Bdim chord"), ("Cmin7dim5", "Cm7dim5 chord")])
+def test_chord_name(name, chord):
+    new_chord = chords.Chord(name)
+    assert repr(new_chord) == chord
+
+
+@pytest.mark.parametrize(
+    "name", [
+        "H#", 10, "Z", len])
+def test_chord_name(name):
+    with pytest.raises(chords.ChordError):
+        new_chord = chords.Chord(name)
+
+
+@pytest.mark.parametrize(
+    "value", [
+        "H#", 10.0, "Z", len])
+def test_chord_transpose_error(value):
+    new_chord = chords.Chord('C')
+    with pytest.raises(TransposeError):
+        new_chord.transpose(value)
+
+
+@pytest.mark.parametrize(
+    "key, value, new_key", [
+        ('C', 3, 'D\u266f'),
+        ('Dm7add5', -5, 'Am7add5'),
+        ('Gmaj', 12, 'G')])
+def test_chord_transpose(key, value, new_key):
+    new_chord = chords.Chord(key)
+    new_chord.transpose(value)
+    assert new_chord.name == new_key

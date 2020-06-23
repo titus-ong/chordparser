6a362a37c6c4784269a7e3187d70ebf2558975af
Titus Ong <55629508+titus-ong@users.noreply.github.com>
2020-06-23 14:37:53 +0800

    Removed __all__

--- src/chordparser/__init__.py
+++ src/chordparser/__init__.py
@@ -3,7 +3,7 @@ A package to parse ChordPro files.
 """
 from chordparser.parser import Parser
 
-__all__ = ['chords.py', 'scales.py', 'notes.py', 'keys.py']
-__author__ = """Titus Ong"""
+
+__author__ = 'Titus Ong'
 __email__ = 'titusongyl@gmail.com'
 __version__ = '0.1.1'

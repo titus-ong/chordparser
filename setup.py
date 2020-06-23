5ac11a60fc61e11f1f2fcff5a0fb6ab2d5e54609
Titus Ong <55629508+titus-ong@users.noreply.github.com>
2020-06-23 14:37:34 +0800

    Updated description, grab info from __init__.py

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 from setuptools import setup, find_packages
+import codecs
+import os.path
+import re
 
 
 def readme():
@@ -6,22 +9,45 @@ def readme():
         return f.read()
 
 
+def fpath(name):
+    return os.path.join(os.path.dirname(__file__), name)
+
+
+def read(fname):
+    return codecs.open(fpath(fname), encoding='utf-8').read()
+
+
+def grep(attrname):
+    pattern = r"{0}\W*=\W*'([^']+)'".format(attrname)
+    strval, = re.findall(pattern, file_text)
+    return strval
+
+
+file_text = read(fpath('src/chordparser/__init__.py'))
+
+
 setup(name='chordparser',
-      version='0.1.1',
+      version=grep('__version__'),
       description='Parse and analyse chords',
       long_description=readme(),
       classifiers=[
           'Development Status :: 3 - Alpha',
           'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
           'Programming Language :: Python :: 3.8',
           'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: 3.6',
           'Topic :: Text Processing',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Utilities',
           ],
-      keywords='chord music parse notation',
+      keywords=(
+          'chords music parse notation analysis '
+          'sheet notes scales keys transpose'
+          ),
       url='http://github.com/titus-ong/chordparser',
-      author='Titus Ong',
-      author_email='titusongyl@gmail.com',
+      author=grep('__author__'),
+      author_email=grep('__email__'),
       license='MIT',
       packages=find_packages(where='src'),
       package_dir={'': 'src'},

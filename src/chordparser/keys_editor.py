2c3d90e912ce70aa7244cf681337c1d5673b7087
Titus Ong <55629508+titus-ong@users.noreply.github.com>
2020-06-24 15:32:08 +0800

    Added method to change key and refactored code to simplify it

--- src/chordparser/keys_editor.py
+++ src/chordparser/keys_editor.py
@@ -6,9 +6,9 @@ from typing import Union
 
 class KeyEditor:
     """
-    KeyEditor class that can create a Key and change keys to their relative major/minor key.
+    KeyEditor class that can create/change a Key, and change keys to their relative major/minor key.
 
-    The KeyEditor class can create a Key using the 'create_key' method by accepting a Note or string, an optional mode (default 'major') and optional submode. Submodes are only available for the minor/aeolian mode (default 'natural'). The 'relative_major' and 'relative_minor' methods change the key into its relative major/minor (submode can be specified for 'relative_minor').
+    The KeyEditor class can create a Key using the 'create_key' method by accepting a Note or string, an optional mode (default 'major') and optional submode. Submodes are only available for the minor/aeolian mode (default 'natural'). The 'relative_major' and 'relative_minor' methods change the key into its relative major/minor (submode can be specified for 'relative_minor'). The 'change_key' method can be used to alter the root, mode and submode all at once.
     """
     _modes = (
         'major', 'minor', 'ionian', 'dorian', 'phrygian',
@@ -37,34 +37,40 @@ class KeyEditor:
         mode -- mode of the key e.g. major (str)
         submode -- alternate forms of the same mode e.g. harmonic, natural (str)
         """
-        # root note
+        root = self._check_root(root)
+        mode = self._check_mode(mode)
+        submode = self._check_submode(mode, submode)
+        return Key(root, mode, submode)
+
+    def _check_root(self, root):
         if not isinstance(root, Note):
             try:
                 NE = NoteEditor()
                 root = NE.create_note(root)
             except TypeError:
                 raise TypeError("Only Notes and strings are accepted for root note")
-        # mode
+        return root
+
+    def _check_mode(self, mode):
         if not isinstance(mode, str):
             raise TypeError("Only strings are accepted for mode")
         if mode.lower() not in KeyEditor._modes:
             raise KeyError("Mode could not be found")
-        mode = mode.lower()
-        # submode
+        return mode.lower()
+
+    def _check_submode(self, mode, submode):
         if submode is None and mode not in KeyEditor._submodes.keys():
-            pass
-        elif submode is None:
-            submode = 'natural'
-        elif not isinstance(submode, str):
+            return submode
+        if submode is None:
+            return 'natural'
+        if not isinstance(submode, str):
             raise TypeError("Only strings are accepted")
-        else:
-            submode_tuple = KeyEditor._submodes.get(mode)
-            if submode_tuple is None:
-                raise KeyError("Mode does not have any submodes")
-            if submode.lower() not in submode_tuple:
-                raise KeyError("Submode could not be found")
-            submode = submode.lower()
-        return Key(root, mode, submode)
+        submode_tuple = KeyEditor._submodes.get(mode)
+        if submode_tuple is None:
+            raise KeyError("Mode does not have any submodes")
+        if submode.lower() not in submode_tuple:
+            raise KeyError("Submode could not be found")
+        return submode.lower()
 
     def relative_major(self, key):
         """Change a key to its relative major."""
@@ -86,4 +92,18 @@ class KeyEditor:
         key.transpose(-3, -2)
         key.submode = submode
         key.mode = 'minor'
+        return key
+
+    def change_key(self, key, root: Union[Note, None] = None, mode: Union[str, None] = None, submode: Union[str, None] = None):
+        """Change the key by specifying root, mode and/or submode."""
+        if not isinstance(key, Key):
+            raise TypeError(f"Object {key} is not a 'Key'")
+        if root:
+            key.root = self._check_root(root)
+        if mode:
+            key.mode = self._check_mode(mode)
+        if not submode and key.mode in KeyEditor._submodes.keys():
+            key.submode = self._check_submode(key.mode, key.submode)
+        else:
+            key.submode = self._check_submode(key.mode, submode)
         return key

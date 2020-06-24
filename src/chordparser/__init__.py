b66fcdc20a61fc1673f6f8b272c06517b4ad726a
Titus Ong <55629508+titus-ong@users.noreply.github.com>
2020-06-24 16:34:02 +0800

    Import musical classes

--- src/chordparser/__init__.py
+++ src/chordparser/__init__.py
@@ -2,8 +2,12 @@
 A package to parse ChordPro files.
 """
 from chordparser.parser import Parser
+from chordparser.notes import Note
+from chordparser.keys import Key
+from chordparser.scales import Scale
+from chordparser.chords import Chord
 
 
 __author__ = 'Titus Ong'
 __email__ = 'titusongyl@gmail.com'
-__version__ = '1.0.0'
+__version__ = '0.2.0'

f1da1df1e1efa11b6072ea9879b264f129ec02dd
Titus Ong <titusongyl@gmail.com>
2020-07-18 23:58:37 +0800

    Initial commit

---
+++ src/chordparser/quality.py
@@ -0,0 +1,182 @@
+class Quality:
+    """
+    Quality class that is built from a chord's quality and provides intervals and scale degrees.
+
+    The Quality class is built from the base chord quality, extensions, and optional flat extension. It has the 'base_intervals' and 'base_degrees' attributes for the intervals and scale degrees of the base chord, and 'intervals' and 'degrees' attributes for that of the proper chord.
+
+    The Quality is represented by its full name by default. The short form of the Quality can be accessed via the 'short' method.
+    """
+    _flat = '\u266d'
+    _sharp = '\u266f'
+    _doubleflat = '\U0001D12B'
+    _doublesharp = '\U0001D12A'
+    _symbols = {
+        -1: _flat, -2: _doubleflat,
+        +1: _sharp, +2: _doublesharp,
+        0: '',
+    }
+    _heptatonic_base = (2, 2, 1, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 1)
+    _base_int = {
+        'power': (7,),
+        'major': (4, 3),
+        'minor': (3, 4),
+        'diminished': (3, 3),
+        'augmented': (4, 4),
+        'dominant': (4, 3),
+        'half-diminished': (3, 3),
+        'sus2': (2, 5),
+        'sus4': (5, 2),
+    }
+    _base_deg = {
+        'power': (1, 5),
+        'sus2': (1, 2, 5),
+        'sus4': (1, 4, 5),
+    }
+    _ext_int = {
+        'seventh': (10,),
+        'ninth': (10, 14),
+        'eleventh': (10, 14, 17),
+        'thirteenth': (10, 14, 17, 21),
+        'major seventh': (11,),
+        'major ninth': (11, 14),
+        'major eleventh': (11, 14, 17),
+        'major thirteenth': (11, 14, 17, 21),
+        'diminished seventh': (9,),
+    }
+    _shortform = {
+        'power': "5",
+        'major': "",
+        'minor': 'm',
+        'diminished': 'dim',
+        'augmented': 'aug',
+        'dominant': "",
+        'half-diminished': "m",
+        'sus2': "sus2",
+        'sus4': "sus",
+    }
+    _ext_shortform = {
+        'seventh': "7",
+        'ninth': "9",
+        'eleventh': "11",
+        'thirteenth': "13",
+        'major seventh': "maj7",
+        'major ninth': "maj9",
+        'major eleventh': "maj11",
+        'major thirteenth': "maj13",
+        'diminished seventh': "7",
+    }
+
+    def __init__(self, quality_str, ext_str=None, flat_ext=False):
+        """Quality that composes of chord quality, chord extension and any alterations to the extension.
+
+        Arguments:
+        quality_str -- chord quality e.g. major, minor (str)
+        ext_str -- chord extension e.g. seventh, major ninth (str)
+        flat_ext -- if flat extension e.g. flat ninth (bool)
+        """
+        self.value = quality_str
+        self.ext = ext_str
+        self.flat_ext = flat_ext
+        self.build()
+
+    def build(self):
+        """Build intervals, scale degrees, and symbols."""
+        self._check()
+        self._base()
+        self._ext()
+        self._sym()
+
+    def _check(self):
+        if not self.ext:
+            return
+        # Check dominant chords do not have a value of 'major'
+        if self.value == "major":
+            if self.ext.split()[0] != "major":
+                raise ValueError("'Major' can either be dominant or major extended chord")
+        # Check diminished chords are only diminished sevenths
+        if self.value == "diminished" or self.ext == "diminished seventh":
+            if self.ext != "diminished seventh" or self.value != "diminished":
+                raise ValueError("Diminished chords can only be seventh")
+        # Check flat extension does not work with sevenths
+        if self.ext.split()[-1] == "seventh" and self.flat_ext:
+            raise ValueError("Flat extension does not apply to seventh chords")
+
+    def _base(self):
+        """Set base intervals and degrees."""
+        self.base_intervals = Quality._base_int[self.value]
+        self.base_degrees = Quality._base_deg.get(self.value, (1, 3, 5))
+
+    def _ext(self):
+        """Set intervals and degrees with chord extensions."""
+        if not self.ext:
+            self.intervals = self.base_intervals
+            self.degrees = self.base_degrees
+            return
+        intervals = list(self.base_intervals)
+        degrees = list(self.base_degrees)
+        for idx, semitone in enumerate(Quality._ext_int[self.ext]):
+            intervals.append(semitone - sum(intervals))
+            degrees.append(7 + 2 * idx)
+        if self.flat_ext:
+            intervals[-1] = intervals[-1] - 1
+        self.intervals = tuple(intervals)
+        self.degrees = tuple(degrees)
+
+    def _sym(self):
+        """Build base symbols and symbols with extension."""
+        symbols = [""]
+        for i in range(len(self.intervals)):
+            sum_interval = sum(self.intervals[:i+1])
+            next_ = self.degrees[i+1]
+            diff = sum(Quality._heptatonic_base[:next_-1])
+            symbols.append(Quality._symbols[sum_interval-diff])
+        if self.value == "power":
+            self.base_symbols = tuple(symbols[0:2])
+        else:
+            self.base_symbols = tuple(symbols[0:3])
+        self.symbols = tuple(symbols)
+
+    def short(self):
+        """Return short form for quality."""
+        string = Quality._shortform[self.value]
+        if not self.ext:
+            return string
+        ext_string = Quality._ext_shortform[self.ext]
+        if self.flat_ext:
+            # add flat symbols
+            if len(ext_string) < 3:
+                ext_string = '\u266d' + ext_string
+            else:
+                ext_string = ext_string[0:3] + '\u266d' + ext_string[3:]
+        if self.value == "half-diminished":
+            ext_string += "\u266d5"
+        if self.value == "augmented":
+            string = ""
+            ext_string += "\u266f5"
+        if self.value in {"sus2", "sus4"}:
+            # sus at the back
+            return ext_string + string
+        return string + ext_string
+
+    def __repr__(self):
+        if not self.ext:
+            return self.value
+        if self.value in {"major", "diminished"}:
+            # avoid word overlap
+            string = self.ext
+        else:
+            string = self.value + " " + self.ext
+        if not self.flat_ext:
+            return string
+        split_string = string.split()
+        split_string.insert(-1, "flat")
+        return " ".join(split_string)
+
+    def __eq__(self, other):
+        if not isinstance(other, Quality):
+            return NotImplemented
+        return (
+            self.value == other.value
+            and self.ext == other.ext
+            and self.flat_ext == other.flat_ext
+            )
---
+++ src/chordparser/quality_editor.py
@@ -0,0 +1,156 @@
+from chordparser.quality import Quality
+import re
+
+
+class QualityEditor:
+    _flat_pattern = '\u266D|\U0001D12B|bb|b'
+    _sharp_pattern = '\u266F|\U0001D12A|##|#'
+    _symbol_pattern = f"{_flat_pattern}|{_sharp_pattern}"
+    _major_pattern = 'Maj|Ma|M|maj|\u0394'
+    _minor_pattern = 'min|m|-'
+    _dim_pattern = 'dim|o|\u00B0'
+    _aug_pattern = r'aug|\+'
+    _halfdim_pattern = '\u00f8|\u00d8'
+    _dom_pattern = 'dom'
+    _minmaj_pattern = f"(?:{_minor_pattern})(?:{_major_pattern})"
+    _augmaj_pattern = f"(?:{_aug_pattern})(?:{_major_pattern})"
+    _power_chord = "5"
+    _extended_str = f"({_flat_pattern}){{0,1}}(7|9|11|13)"
+    _altered_5 = f"({_dim_pattern}|{_aug_pattern}|{_symbol_pattern})5"
+    _suspended = f"sus(2|4){{0,1}}"
+    quality_pattern = (
+        f"(({_power_chord})|"
+        f"((({_minmaj_pattern})|({_augmaj_pattern})|"
+        f"({_aug_pattern})|({_dim_pattern})|"
+        f"({_halfdim_pattern})|({_major_pattern})|"
+        f"({_minor_pattern})){{0,1}}"  # 0 for dominant/minor ext (lowercase note)
+        f"({_extended_str}))|"
+        f"(({_aug_pattern})|({_dim_pattern})|"
+        f"({_major_pattern})|({_minor_pattern}))){{0,1}}"
+        f"(?:{_altered_5}){{0,1}}"
+        f"({_suspended}){{0,1}}"
+    )
+
+    # Pattern notation [regex group]
+    # Match string [0]:
+    #     {
+    #         power chord [2] |
+    #         extended chords [3] {
+    #             (
+    #                 dominant [4-none; minor if note is lowercase] |
+    #                 minmaj [5] |
+    #                 augmaj [6] |
+    #                 aug [7] |
+    #                 dim [8] |
+    #                 halfdim [9] |
+    #                 major [10] |
+    #                 minor [11]
+    #                 ) +
+    #             (symbol [13] + degree [14]) [12]
+    #             } |
+    #         triads [15] (
+    #             aug [16] |
+    #             dim [17] |
+    #             major [18] |
+    #             minor [19]
+    #             )
+    #         } [1],
+    #         altered 5ths [20],
+    #         sus [21] 2/4 [22]
+
+    def create_quality(self, string, capital_note=True):
+        """Return chord quality."""
+        if string is None:
+            if capital_note:
+                return Quality("major")
+            return Quality("minor")
+        rgx = re.match(QualityEditor.quality_pattern, string, re.UNICODE)
+        if rgx.group(2):  # power
+            return Quality("power")
+        alt5 = self._parse_alt5(rgx.group(20))
+        quality_str = self._parse_base(rgx, alt5, capital_note)
+        ext_str = self._parse_ext(rgx, alt5, capital_note)
+        flat_ext = self._parse_flat_ext(rgx.group(13))
+        return Quality(quality_str, ext_str, flat_ext)
+
+    def _parse_alt5(self, alt5):
+        """Return state of altered 5: 0 if none, -1 if flat 5, 1 if aug 5."""
+        if not alt5:
+            return 0
+        if alt5 in QualityEditor._dim_pattern + QualityEditor._flat_pattern:
+            return -1
+        return 1
+
+    def _parse_base(self, rgx, alt5, capital_note):
+        if rgx.group(21):
+            return self._parse_sus(rgx)
+        if rgx.group(15):
+            return self._parse_triad(rgx, alt5)
+        if rgx.group(3):  # extended chord
+            return self._parse_base_ext(rgx, alt5, capital_note)
+        if not capital_note:
+            return "diminished" if alt5 < 0 else "minor"
+        return "augmented" if alt5 > 0 else "major"
+
+    def _parse_sus(self, rgx):
+        note = rgx.group(22) or "4"  # if 'sus'
+        return "sus" + note
+
+    def _parse_triad(self, rgx, alt5):
+        if rgx.group(16):
+            return "augmented"
+        if rgx.group(17):
+            return "diminished"
+        if rgx.group(18):
+            if alt5 > 0:
+                return "augmented"
+            return "major"
+        if alt5 < 0:
+            return "diminished"
+        return "minor"
+
+    def _parse_base_ext(self, rgx, alt5, capital_note):
+        """Parse base quality of extended chords."""
+        if not rgx.group(4):
+            if capital_note:
+                quality = "dominant"
+            else:
+                quality = "minor"
+        elif rgx.group(5) or rgx.group(11):
+            quality = "minor"
+        elif rgx.group(6) or rgx.group(7):
+            quality = "augmented"
+        elif rgx.group(8):
+            quality = "diminished"
+        elif rgx.group(9):
+            quality = "half-diminished"
+        elif rgx.group(10):
+            quality = "major"
+        if alt5 < 0 and quality == "minor":
+            quality = "half-diminished"
+        if alt5 > 0 and quality in {"major", "dominant"}:
+            quality = "augmented"
+        return quality
+
+    def _parse_ext(self, rgx, alt5, capital_note):
+        """Parse quality of extended chords."""
+        if not rgx.group(3):
+            return None
+        num_to_words = {
+            '7': "seventh",
+            '9': "ninth",
+            '11': "eleventh",
+            '13': "thirteenth",
+        }
+        ext_str = ""
+        if rgx.group(5) or rgx.group(6) or rgx.group(10):
+            ext_str += "major "
+        if rgx.group(8):
+            ext_str += "diminished "
+        ext_str += num_to_words[rgx.group(14)]
+        return ext_str
+
+    def _parse_flat_ext(self, flat):
+        if flat:
+            return True
+        return False

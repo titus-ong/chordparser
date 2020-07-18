8b276c4e133aa490d1a1d82c2dc3eb9cc5f09823
Titus Ong <titusongyl@gmail.com>
2020-07-19 01:46:38 +0800

    Move roman code to CRC class

--- src/chordparser/chords_analyser.py
+++ src/chordparser/chords_analyser.py
@@ -1,7 +1,8 @@
 from chordparser.notes_editor import NoteEditor
 from chordparser.scales_editor import ScaleEditor
 from chordparser.chords_editor import ChordEditor
-from typing import Union, List
+from chordparser.chord_roman_converter import ChordRomanConverter
+from typing import List
 import warnings
 
 
@@ -11,36 +12,6 @@ class ChordAnalyser:
 
     The ChordAnalyser can return the roman numeral notation of a chord using the 'roman' method. It also analyses the chord's relationship to a scale using the 'analyse_diatonic' and 'analyse_all' methods, which account for only diatonic chords and chords from all modes respectively.
     """
-    intervals = {
-        (4, 3): str.upper,
-        (3, 4): str.lower,
-        (3, 3): str.lower,
-        (4, 4): str.upper,
-    }
-    symbols = {
-        -1: '\u266d', -2: '\U0001D12B',
-        +1: '\u266f', +2: '\U0001D12A',
-        0: '',
-        }
-    inversions = {
-        (1, 3): '6', (2, 3): '64',
-        (1, 4): '65', (2, 4): '43',
-        (3, 4): '42',
-    }
-    roman_deg = {
-        1: 'I', 2: 'II', 3: 'III',
-        4: 'IV', 5: 'V', 6: 'VI', 7: 'VII',
-    }
-    q_dict = {
-        'major': 'M',
-        'minor': '',
-        'diminished': '\u00B0',
-        'augmented': '+',
-        'dominant': '',
-        'minor-major': 'M',
-        'half-diminished': '\u00f8',
-        'augmented-major': '+M',
-    }
     mode_list = [
         'major',
         'minor',
@@ -53,35 +24,8 @@ class ChordAnalyser:
     NE = NoteEditor()
     CE = ChordEditor()
     SE = ScaleEditor()
+    CRC = ChordRomanConverter()
 
-    def roman(self, chord, scale):
-        """Return roman numeral notation."""
-        if chord.quality == 'power':
-            raise ValueError("Cannot parse power chords")
-        if chord.sus:
-            warnings.warn("Warning: sus chords may not be parsed correctly", UserWarning)
-        c_int = (self.NE.get_intervals(*chord.base_chord[0:3]))
-        scale = self.SE.create_scale(scale.key.root, "major")
-        degree = min(scale.notes.index(x) for x in scale.notes if x.letter()==chord.root.letter())+1
-        q_fn = self.intervals[c_int]
-        c_qual = q_fn(self.roman_deg[degree])  # lower/uppercase numeral
-        (shift,) = self.NE.get_min_intervals(scale.notes[degree-1], chord.root)
-        symb = self.symbols[shift]  # accidental of root
-        notes = len(chord.base_tones)
-        if chord.bass and chord.tones[0] in chord.base_tones:
-            inversion = chord.base_tones.index(chord.tones[0])
-            inv_str = self.inversions.get((inversion, notes), str(notes*2-1))  # inversion notation
-        elif notes > 3:
-            inv_str = str(notes*2-1)
-        else:
-            inv_str = ''
-        qual = chord.quality.split()[0]
-        if notes > 3 or qual == 'augmented' or qual == 'diminished':
-            q_str = self.q_dict[qual]  # quality notation
-        else:
-            q_str = ''
-        numeral = symb+c_qual+q_str+inv_str
-        return numeral
 
     def analyse_diatonic(self, chord, scale, incl_submodes: bool = False) -> List[tuple]:
         """Return all possible chord function (None if not found). Format: List[(Roman numeral, scale mode, scale submode)]."""

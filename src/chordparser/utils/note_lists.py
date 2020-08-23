da4ac02a1923f54ca1480dc0522bba7ff700e741
Titus Ong <titusongyl@gmail.com>
2020-08-23 21:14:08 +0800

    Minor ordering change

--- src/chordparser/utils/note_lists.py
+++ src/chordparser/utils/note_lists.py
@@ -5,6 +5,18 @@ natural_notes = (
     "C", "D", "E", "F", "G", "A", "B",
     "C", "D", "E", "F", "G", "A", "B",
 )
+sharp_scale = (
+    "C", f"C{sharp}", "D", f"D{sharp}", "E", "F", f"F{sharp}", "G",
+    f"G{sharp}", "A", f"A{sharp}", "B",
+    "C", f"C{sharp}", "D", f"D{sharp}", "E", "F", f"F{sharp}", "G",
+    f"G{sharp}", "A", f"A{sharp}", "B",
+    )
+flat_scale = (
+    "C", f"D{flat}", "D", f"E{flat}", "E", "F", f"G{flat}", "G",
+    f"A{flat}", "A", f"B{flat}", "B",
+    "C", f"D{flat}", "D", f"E{flat}", "E", "F", f"G{flat}", "G",
+    f"A{flat}", "A", f"B{flat}", "B",
+    )
 mode_order = {
     "major": 0,
     "ionian": 0,
@@ -28,15 +40,3 @@ melodic_intervals = (
     0, 0, 0, 0, 1, 0, -1,
     0, 0, 0, 0, 1, 0, -1,
 )
-sharp_scale = (
-    "C", f"C{sharp}", "D", f"D{sharp}", "E", "F", f"F{sharp}", "G",
-    f"G{sharp}", "A", f"A{sharp}", "B",
-    "C", f"C{sharp}", "D", f"D{sharp}", "E", "F", f"F{sharp}", "G",
-    f"G{sharp}", "A", f"A{sharp}", "B",
-    )
-flat_scale = (
-    "C", f"D{flat}", "D", f"E{flat}", "E", "F", f"G{flat}", "G",
-    f"A{flat}", "A", f"B{flat}", "B",
-    "C", f"D{flat}", "D", f"E{flat}", "E", "F", f"G{flat}", "G",
-    f"A{flat}", "A", f"B{flat}", "B",
-    )

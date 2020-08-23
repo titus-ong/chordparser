59c68e44af1d17b21c702acea9d0a74f1a02adae
Titus Ong <titusongyl@gmail.com>
2020-08-23 12:39:35 +0800

    Included mode patterns

--- src/chordparser/utils/regex_patterns.py
+++ src/chordparser/utils/regex_patterns.py
@@ -2,6 +2,13 @@ from chordparser.utils.unicode_chars import (flat, doubleflat,
                                              sharp, doublesharp)
 
 
-note_pattern = "[a-gA-G]"
+note_pattern = "[A-G]"
 flat_pattern = f"bb|b|{flat}|{doubleflat}"
 sharp_pattern = f"##|#|{sharp}|{doublesharp}"
+submode_pattern = "natural|harmonic|melodic"
+mode_pattern = (
+    "major|minor|ionian|dorian|phrygian|"
+    "lydian|mixolydian|aeolian|locrian"
+)
+short_minor_pattern = "m"
+short_major_pattern = ""

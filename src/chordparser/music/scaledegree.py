e6a51d027f93424f20fc4dd5a02d0fb7d240d271
Titus Ong <titusongyl@gmail.com>
2020-08-23 21:13:39 +0800

    Initial commit

---
+++ src/chordparser/music/scaledegree.py
@@ -0,0 +1,109 @@
+from chordparser.music.notationparser import NotationParser
+from chordparser.music.symbol import Symbol
+from chordparser.utils.regex_patterns import (symbol_pattern,
+                                              degree_pattern)
+
+
+class ScaleDegreeNotationParser(NotationParser):
+    """Parse scale degree notation into degree and symbol."""
+
+    _pattern = f"({symbol_pattern})?({degree_pattern})"
+
+    def _split_into_groups(self, regex):
+        symbol = regex.group(1)
+        degree = regex.group(2)
+        return degree, symbol
+
+class ScaleDegree:
+    """A class representing a scale degree.
+
+    The `ScaleDegree` consists of an integer (the degree) and a
+    Symbol. The degree has to be between 1 and 7, and the symbols
+    allowed are b, bb, #, ## or their respective unicode
+    characters \u266d, \u266f, \U0001D12B, or \U0001D12A. Upon
+    creation, the symbol will be converted to unicode.
+
+    Attributes
+    ----------
+    degree : int
+        The degree of the `ScaleDegree`.
+    symbol : Symbol
+        The accidental of the `ScaleDegree`.
+
+    Examples
+    --------
+    >>> sd = ScaleDegree(1, "b")
+    >>> sd
+    \u266d1 scale degree
+
+    """
+
+    _SDNP = ScaleDegreeNotationParser()
+
+    def __init__(self, notation):
+        degree, symbol = self._SDNP.parse_notation(notation)
+        self._degree = int(degree)
+        self._symbol = Symbol(symbol)
+
+    @property
+    def degree(self):
+        return self._degree
+
+    @property
+    def symbol(self):
+        return self._symbol
+
+    def __str__(self):
+        return f"{self._symbol}{self._degree}"
+
+    def __repr__(self):
+        return f"{str(self)} scale degree"
+
+    def __eq__(self, other):
+        """Compare with other `ScaleDegrees` or strings.
+
+        If the other object is a `ScaleDegree`, it must have the same
+        degree and symbol as this `ScaleDegree` to be equal. If it is a
+        string, they must have the same string representation.
+
+        Parameters
+        ----------
+        other
+            The object to be compared with.
+
+        Returns
+        -------
+        boolean
+            The outcome of the comparison.
+
+        Examples
+        --------
+        >>> sd = ScaleDegree("b5")
+        >>> sd2 = ScaleDegree("b5")
+        >>> sd == sd2
+        True
+        >>> sd3 = ScaleDegree("5")
+        >>> sd == sd3
+        False
+        >>> sd4 = ScaleDegree("b4")
+        >>> sd == sd4
+        False
+
+        Note that Symbols only use unicode characters when comparing
+        with other strings.
+
+        >>> sd = ScaleDegree("#7")
+        >>> sd == "#7"
+        False
+        >>> sd == "\u266f7"
+        True
+
+        """
+        if isinstance(other, ScaleDegree):
+            return (
+                self._degree == other.degree and
+                self._symbol == other.symbol
+            )
+        if isinstance(other, str):
+            return str(self) == other
+        return NotImplemented
---
+++ tests/test_music/test_scaledegree.py
@@ -0,0 +1,52 @@
+import pytest
+
+from chordparser.music.scaledegree import (ScaleDegree,
+                                           ScaleDegreeNotationParser)
+from chordparser.music.symbol import Symbol
+
+
+class TestSDNPParseNotation:
+    @pytest.fixture
+    def parser(self):
+        return ScaleDegreeNotationParser()
+
+    def test_correct_groups(self, parser):
+        degree, symbol = parser.parse_notation("5")
+        assert "5" == degree
+        assert None == symbol
+
+    def test_correct_groups_2(self, parser):
+        degree, symbol = parser.parse_notation("b4")
+        assert "4" == degree
+        assert "b" == symbol
+
+    def test_wrong_notation(self, parser):
+        with pytest.raises(SyntaxError):
+            parser.parse_notation("0")
+
+
+class TestScaleDegree:
+    def test_creation(self):
+        sd = ScaleDegree("b5")
+        assert Symbol("b") == sd.symbol
+        assert 5 == sd.degree
+
+
+class TestScaleDegreeEquality:
+    def test_equality_sd(self):
+        sd = ScaleDegree("b5")
+        sd2 = ScaleDegree("b5")
+        assert sd2 == sd
+
+    def test_equality_str(self):
+        sd = ScaleDegree("b5")
+        assert "\u266d5" == sd
+
+    def test_inequality(self):
+        sd = ScaleDegree("b5")
+        sd2 = ScaleDegree("b4")
+        assert sd2 != sd
+
+    def test_inequality_2(self):
+        sd = ScaleDegree("b5")
+        assert len != sd

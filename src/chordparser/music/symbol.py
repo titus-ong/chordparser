38247d81c79d6d929bc11f4a80e69316f7dcd96b
Titus Ong <titusongyl@gmail.com>
2020-08-23 21:12:53 +0800

    Added check for valid symbol

--- src/chordparser/music/symbol.py
+++ src/chordparser/music/symbol.py
@@ -13,6 +13,8 @@ class Symbol(UserString):
 
     """
     def __init__(self, data):
+        if data not in symbol_to_unicode.keys():
+            raise ValueError(f"{data} is not a valid Symbol")
         self.data = symbol_to_unicode[data]
 
     def as_int(self):
